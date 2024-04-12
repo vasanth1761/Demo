b1455060c12ef390482bb4895ff30833e058ca6f
Vasa3558 <Vasa3558@CSL0701.global.chainsys.com>
2024-04-12 14:54:54 +0530

    updated int

---
+++ chainsys/src/com/demo1/helloworld.java
@@ -0,0 +1,11 @@
+package com.demo1;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("hello world");
+     int c;
+	}
+
+}
